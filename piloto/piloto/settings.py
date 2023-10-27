2031a24237839cd235b39c32234ec81509a0a41a
Fernanda Menezes <fernanda.plessim@mail.uft.edu.br>
2023-10-27 12:11:11 -0300

    Gráficos adicionados

--- piloto/piloto/settings.py
+++ piloto/piloto/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/piloto/static/'
 
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'piloto/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
